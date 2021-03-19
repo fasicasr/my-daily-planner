a67929e2dc9460a1a437dcfdadbc1dd131b83cdd
Fasica Robi <fasicasr@gmail.com>
2021-03-19 01:48:25 -0400

    updated js file

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//display of current time 
 let date = new Date();
-let now = moment(date).format('dddd MMMM Do YYYY');
+let now = moment(date).format('dddd, MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = now;
 
+let textValue;
 //currentTime will get the value of hour(h)
-let currentTime =  moment(date).format('h');
+// let currentTime =  moment(date).format('h');
+let currentTime = 2;
 let timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 let timeIndex;
 //matching currentTime with timeArray[timeIndex]
@@ -17,7 +20,7 @@ if (currentTime == 9) {
     timeIndex = 3;
 }else if (currentTime == 1) {
     timeIndex = 4;
-}else if (currentTime == 2) {
+}else if (currentTime == 2) {3
     timeIndex = 5;
 }else if (currentTime == 3) {
     timeIndex = 6;
@@ -26,30 +29,56 @@ if (currentTime == 9) {
 }else if (currentTime == 5) {
     timeIndex = 8;
 }
-
+let i;
 //used +timeArray+ to get the value of the variable(timeArray)
-for (let i = 0; i < timeArray.length; i++) {
+for (i = 0; i < timeArray.length; i++) {
     //if timeArray is <= 11am display rows gray 
     //else if timeArray === 12pm display row red 
     //else display green 
+    //<textarea> element is used to collect user input. multi-line plain-text editing control
     if (i < timeIndex ) { //prints gray(past)
-        $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row time-block">\
+        // textValue = localStorage.getItem(localStorage.key());
+        textValue = localStorage.getItem(i);
+        $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row past time-block">\
         <div class="col-md-1 hour">'+timeArray[i]+'</div>\
-        <div class="col-md-10">notes</div>\
-        <button class="btn saveBtn col-md-1">Save</button>\
+        <textarea class="col-md-10" id="textStorage">'+textValue+'</textarea>\
+        <button class="btn saveBtn col-md-1" id="'+i+'">Save</button>\
         </div>'); 
+        
     }else if (i === timeIndex){//prints red(current)
-        $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row2 time-block">\
+        textValue = localStorage.getItem(i);
+        $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row present time-block">\
         <div class="col-md-1 hour">'+timeArray[i]+'</div>\
-        <div class="col-md-10">notes</div>\
-        <button class="btn saveBtn col-md-1">Save</button>\
+        <textarea class="col-md-10" id="textStorage">'+textValue+'</textarea>\
+        <button class="btn saveBtn col-md-1" id="'+i+'">Save</button>\
         </div>'); 
+       
     }else {//prints green (future)
-        $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row3 time-block">\
+        textValue = localStorage.getItem(i);
+        $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row future time-block">\
         <div class="col-md-1 hour">'+timeArray[i]+'</div>\
-        <div class="col-md-10">notes</div>\
-        <button class="btn saveBtn col-md-1">Save</button>\
+        <textarea class="col-md-10" id="textStorage">'+textValue+'</textarea>\
+        <button class="btn saveBtn col-md-1" id="'+i+'">Save</button>\
         </div>'); 
+        
     }
 }
+
+
+$(document).ready(function(){
+    $(".saveBtn").on("click", function() {
+        localStorage.setItem($(this).prop("id"), $(this).prev().val());
+        // console.log(localStorage.getItem($(this).prop("id")));  
+    })
+});
+
+
+
+
+
+
+
+
+
+
 
