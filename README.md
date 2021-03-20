b5da7da5bb0d64184c476bc879c1ebd6a8e4ec47
Fasica Robi <fasicasr@gmail.com>
2021-03-20 19:26:11 -0400

    modified css

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# my-daily-planner
+# My Daily Planner
+
+Users can use this simple application to plan their daily work and/or life schedule. Users are able to save events for each hour of the day so that they can manage their time effectively. 
+
+# Planner Details
+
+* Users are able to open up the planner and seee times highlighed in different color to indicate past, present and future. 
+* After typing an event on the time slots, users are able to save thier event by clicking the bottom on the right of the screen.
+* Users are able to refresh the page to see their saved events.
+
+# Webpage Demo
+
+* FSR CODE QUIZ Demo 
+![webpage image](./imges/homepge.png)
+
+![webpage image](./images/quizpae.png)
+
+![webpage image](./images/savescorepae.png)
+
+![webpage image](./images/highscorespge.png)
+
+
+# Webpage 
+
+Application URL: 
+Github: 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//display of current time 
+//display of current time. Format from momment.com
 let date = new Date();
 let now = moment(date).format('dddd, MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = now;
@@ -6,7 +6,7 @@ document.getElementById("currentDay").innerHTML = now;
 let textValue;
 //currentTime will get the value of hour(h)
 // let currentTime =  moment(date).format('h');
-let currentTime = 3;
+let currentTime = 12; //use when testing a hours past 9-5pm
 let timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 let timeIndex;
 //matching currentTime with timeArray[timeIndex]
@@ -35,7 +35,7 @@ for (i = 0; i < timeArray.length; i++) {
     //if timeArray is <= 11am display rows gray 
     //else if timeArray === 12pm display row red 
     //else display green 
-    //<textarea> element is used to collect user input. multi-line plain-text editing control
+    //<textarea> element is used to collect user input
     if (i < timeIndex ) { //prints gray(past)
         textValue = localStorage.getItem(i);
         if (textValue === null){
@@ -44,7 +44,7 @@ for (i = 0; i < timeArray.length; i++) {
         $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row past time-block">\
         <div class="col-md-1 hour">'+timeArray[i]+'</div>\
         <textarea class="col-md-10" id="textStorage">'+textValue+'</textarea>\
-        <button class="btn saveBtn col-md-1" id="'+i+'">Save</button>\
+        <button class="saveBtn col-md-1 fa fa-save" id="'+i+'"></button>\
         </div>'); 
         
     }else if (i === timeIndex){//prints red(current)
@@ -55,8 +55,8 @@ for (i = 0; i < timeArray.length; i++) {
         $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row present time-block">\
         <div class="col-md-1 hour">'+timeArray[i]+'</div>\
         <textarea class="col-md-10" id="textStorage">'+textValue+'</textarea>\
-        <button class="btn saveBtn col-md-1" id="'+i+'">Save</button>\
-        </div>'); 
+        <button class="saveBtn col-md-1 fa fa-save" id="'+i+'"></button>\
+        </div>'); //found the save icon from fontawesome
        
     }else {//prints green (future)
         textValue = localStorage.getItem(i);
@@ -66,17 +66,16 @@ for (i = 0; i < timeArray.length; i++) {
         $( ".container" ).append ('<div id="'+timeArray[i]+'" class="row future time-block">\
         <div class="col-md-1 hour">'+timeArray[i]+'</div>\
         <textarea class="col-md-10" id="textStorage">'+textValue+'</textarea>\
-        <button class="btn saveBtn col-md-1" id="'+i+'">Save</button>\
+        <button class="saveBtn col-md-1 fa fa-save" id="'+i+'"></button>\
         </div>'); 
         
     }
 }
 
-
+//retrieving/storing events
 $(document).ready(function(){
     $(".saveBtn").on("click", function() {
         localStorage.setItem($(this).prop("id"), $(this).prev().val());
-        // console.log(localStorage.getItem($(this).prop("id")));  
     })
 });
 
